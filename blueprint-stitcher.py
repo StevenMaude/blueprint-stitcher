abefc773296dbe9048068ef85489c17dde1c82cd
Steven Maude <steve@github.ubuntu>
2015-05-20 17:34:33 +0100

    Initial commit of working script

---
+++ blueprint-stitcher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+import os
+import importlib
+from app import flask_app
+
+
+blueprint_names = [f for f in os.listdir('app/blueprint')
+                   if os.path.isdir(os.path.join('app/blueprint', f))]
+
+for blueprint_name in blueprint_names:
+    module_name = "app.blueprint.{0}.{0}".format(blueprint_name)
+    module = importlib.import_module(module_name)
+    blueprint = getattr(module, blueprint_name)
+    flask_app.register_blueprint(blueprint)
+
+flask_app.run()
