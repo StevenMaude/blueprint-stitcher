6e51c53f841defec77e0d25db771084a36fc78b4
Steven Maude <git@stevenmaude.co.uk>
2015-05-26 14:19:13 +0100

    Give blueprint apps a URL prefix of their name
    So we don't get (or have to worry about) unwanted path clashes.

--- blueprint-stitcher.py
+++ blueprint-stitcher.py
@@ -6,7 +6,6 @@ import os
 import importlib
 from app import flask_app
 
-
 blueprint_names = [f for f in os.listdir('app/blueprint')
                    if os.path.isdir(os.path.join('app/blueprint', f))]
 
@@ -14,6 +13,6 @@ for blueprint_name in blueprint_names:
     module_name = "app.blueprint.{0}.{0}".format(blueprint_name)
     module = importlib.import_module(module_name)
     blueprint = getattr(module, blueprint_name)
-    flask_app.register_blueprint(blueprint)
+    flask_app.register_blueprint(blueprint, url_prefix='/' + blueprint_name)
 
 flask_app.run()
