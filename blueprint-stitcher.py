e10fb00aa545b375ea97cc636f28e9489aef7655
Steven Maude <git@stevenmaude.co.uk>
2015-05-26 15:54:45 +0100

    Restructure blueprint location
    Current idea: clone a repo into blueprint that contains an app
    directory, which in turn contains the views module to import.

--- blueprint-stitcher.py
+++ blueprint-stitcher.py
@@ -12,7 +12,7 @@ blueprint_names = [f for f in os.listdir('blueprint')
                    if os.path.isdir(os.path.join('blueprint', f))]
 
 for blueprint_name in blueprint_names:
-    module_name = "app.blueprint.{0}.{0}".format(blueprint_name)
+    module_name = "blueprint.{0}.app.{0}".format(blueprint_name)
     module = importlib.import_module(module_name)
     blueprint = getattr(module, blueprint_name)
     flask_app.register_blueprint(blueprint, url_prefix='/' + blueprint_name)
